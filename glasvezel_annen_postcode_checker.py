f11ab3e7c43a1091828353b92a1afa49851744c0
pwieringa <peter.wieringa@gmail.com>
2019-01-18 15:27:05 +0100

    output to file

--- glasvezel_annen_postcode_checker.py
+++ glasvezel_annen_postcode_checker.py
@@ -1,33 +1,37 @@
 import requests
 import time
+from datetime import datetime
 from postcodes import postcodes
 
 counter = 0
 
 base_url = 'https://www.deltaglasvezel.nl/app/actiecode/checkadres.web'
-f = open('glasvezel.txt', 'w')
+sleep = 1500
 
 for postcode in postcodes:
     for huisnummer in range(int(postcode[1]), int(postcode[2]), 1):
+        f = open('glasvezel.txt', 'w')
         endpoint = '?zipcode={}&housenumber={}'.format('9999AA', '1')
         url = base_url + endpoint
         r = requests.post(url)
         if r.text[-35:-6] == 'maximaalaantalpogingenbereikt':
-            print('IP geblokkeerd, 15 minuten pauze')
-            time.sleep(1800)
+            line = 'IP geblokkeerd, {} minuten pauze'.format(sleep/60)
+            print('{} - {}'.format(datetime.now(), line))
+            f.write('{} - {}'.format(datetime.now(), line))
+            f.close()
+            time.sleep(sleep)
         else:
             endpoint = '?zipcode={}&housenumber={}'.format(postcode[0], str(huisnummer))
             url = base_url + endpoint
             r = requests.post(url)
             if r.text[-34:-6] == 'bestellingbijandereaanbieder':
                 counter += 1
-                line = ('{}, {}, {}, Caiway\n'.format(str(counter), postcode[0], str(huisnummer)))
-                print(line)
-                f.write(str(line))
+                line = ('{} - {}, {}, {}, Caiway'.format(datetime.now(), str(counter), postcode[0], str(huisnummer)))
+                f.write(line)
             elif r.text[-34:-6] == 'kanordernietwijzigenviaadres':
                 counter += 1
-                line = ('{}, {}, {}, Delta\n'.format(str(counter), postcode[0], str(huisnummer)))
+                line = ('{} - {}, {}, {}, Delta'.format(datetime.now(), str(counter), postcode[0], str(huisnummer)))
                 print(line)
-                f.write(str(line))
-
+                f.write(line)
+            f.close()
 f.close()
